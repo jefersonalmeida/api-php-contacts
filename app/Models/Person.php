0fb38d2a9c503a551962666260c367d9e92622db
Jeferson Almeida <jeferson.almeida@outlook.com>
2021-02-28 16:06:33 -0400

    Cria model para pessoa

---
+++ app/Models/Person.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Jas\Models;
+
+use Eloquent;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Jas\Models\Traits\UuidTrait;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+class Person extends Model implements Transformable
+{
+    use TransformableTrait;
+    use UuidTrait;
+
+    public $timestamps = false;
+    protected $table = 'public.persons';
+    protected $primaryKey = 'id';
+    protected $keyType = 'string';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * @return HasMany
+     */
+    public function contacts(): HasMany
+    {
+        return $this->hasMany(Contact::class, 'person_id', 'id');
+    }
+}
