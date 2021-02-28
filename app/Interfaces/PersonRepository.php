29fcd9e27c76d9cef523be1eb7f0a77a00fb14ec
Jeferson Almeida <jeferson.almeida@outlook.com>
2021-02-28 16:04:45 -0400

    Cria interface para pessoa

---
+++ app/Interfaces/PersonRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Jas\Interfaces;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface PersonRepository.
+ *
+ * @package namespace Jas\Repositories;
+ */
+interface PersonRepository extends RepositoryInterface
+{
+}
