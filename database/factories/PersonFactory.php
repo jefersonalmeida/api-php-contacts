9fd8d17ad79ae83051c771d3ee4824ae2c9d3cd1
Jeferson Almeida <jeferson.almeida@outlook.com>
2021-02-28 16:09:25 -0400

    Cria pipeline para github actions

---
+++ database/factories/PersonFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Jas\Models\Person;
+
+class PersonFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Person::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+        ];
+    }
+}
